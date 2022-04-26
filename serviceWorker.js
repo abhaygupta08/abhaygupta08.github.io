4ef7928a6e4207c8c22924798a80577572f9ef91
Abhay Gupta <abhay.hybrowlabs@gmail.com>
2022-04-27 02:34:02 +0530

    feature: PWA

---
+++ serviceWorker.js
@@ -0,0 +1,50 @@
+const staticDevCoffee = "abhay-gupta-portfolio-v1"
+const assets = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/contact.html",
+    "/about.html",
+    "/contact.css",
+    "/CNAME",
+    "/boiler.css",
+    "/animation.css",
+    "/about.css",
+    "/fonts/VisueltPro-Bold.e06be4a7d7345624af47aadbd145bddc.woff2",
+    "/fonts/VisueltPro-Regular.f00251d89110a6d8cac2cc1ad91a9e03.woff2",
+    "/config/pp-removebg-preview.png",
+    "/config/resume.pdf",
+    "/assets/vertical_align_bottom_white_24dp.svg",
+    "/assets/twitter.svg",
+    "/assets/linkedin.svg",
+    "/assets/github.svg",
+    "/assets/instagram.svg",
+    "/assets/paper-plane-svgrepo-com.svg",
+    "/assets/mail.svg",
+    "/assets/favicon.png",
+    "/assets/favicon.ico",
+    "/assets/cp-buddy.png",
+    "/assets/ceasar-bust.c2bf68cb3b24b5361c95d04c445511a8.png",
+    "/assets/abhay.svg",
+    "/assets/abhay2.svg",
+    "/assets/icons/icon-192x192.png",
+    "/assets/icons/icon-256x256.png",
+    "/assets/icons/icon-384x384.png",
+    "/assets/icons/icon-512x512.png",
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(abhayGuptaPortfolio).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
